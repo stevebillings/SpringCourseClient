43f30133fcb7b6e0ff92934f30b26ae34fa2f36e
sbillings <sbillings@blackducksoftware.com>
2016-02-28 14:19:17 -0500

    refined app

--- src/com/warehouse/WarehouseApp.java
+++ src/com/warehouse/WarehouseApp.java
@@ -1,6 +1,8 @@
 package com.warehouse;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,6 +10,8 @@ import com.warehouse.rest.client.Warehouse;
 import com.warehouse.rest.model.Product;
 
 public class WarehouseApp {
+
+	private static final int PRODUCT_QUANTITY_INCREMENT = 100;
 
 	public static void main(String[] args) throws Exception {
 		WarehouseApp app = new WarehouseApp();
@@ -19,13 +23,36 @@ public class WarehouseApp {
 				"/META-INF/spring/WarehouseContext.xml");
 		Warehouse client = (Warehouse) context.getBean("warehouse");
 
+		Map<String, Integer> origQuantities = new HashMap<>();
+
 		// Get products
 		System.out.println("\n==== Get Products ...");
 		List<Product> products = client.getProducts();
 		System.out.println("---- Total products=" + products.size());
 		for (int i = 0; i < products.size(); i++) {
-			System.out.println("product[" + i + "]=" + products.get(i));
-			client.addToProductQuantity(products.get(i).getProductId(), 100);
+			Product product = products.get(i);
+			System.out.println("product[" + i + "]=" + product);
+			origQuantities.put(product.getProductName(), product.getAvailableQuantity());
+
+			// Add to the quantity
+			client.addToProductQuantity(product.getProductId(), PRODUCT_QUANTITY_INCREMENT);
+		}
+
+		// Get the now-re-stocked products
+		System.out.println("\n==== Get Products after restocking...");
+		products = client.getProducts();
+		System.out.println("---- Total products=" + products.size());
+		for (int i = 0; i < products.size(); i++) {
+			Product product = products.get(i);
+			System.out.println("product[" + i + "]=" + product);
+			int expectedQuantity = origQuantities.get(product.getProductName()) + PRODUCT_QUANTITY_INCREMENT;
+			int actualQuantity = product.getAvailableQuantity();
+			if (actualQuantity == expectedQuantity) {
+				System.out.println(product.getProductName() + ": New quantity (" + actualQuantity + ") is correct");
+			} else {
+				System.out.println("\n\n\nERROR: " + product.getProductName() + ": Expected quantity "
+						+ expectedQuantity + ", but got: " + actualQuantity);
+			}
 		}
 
 		// Get products by id
